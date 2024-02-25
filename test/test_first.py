58025764db983b36b233f3329729c0714f0e31bf
Jean Carlo Machado <jean.machado@getyourguide.com>
2024-02-25 04:36:35 +0100

    improvements

--- test/test_first.py
+++ test/test_first.py
@@ -7,3 +7,6 @@ def test_first():
 
     dwh = DataWharehouse('/tmp/events')
     dwh.write_event('Test', {'foo': 'bar'})
+    dwh.write_event('Test', {'foo': 'baz'})
+    result =dwh.event('Test')
+    print('Loaded event', result)
--- tiny_data_wharehouse/data_wharehouse.py
+++ tiny_data_wharehouse/data_wharehouse.py
@@ -1,13 +1,54 @@
-
+import os
 
 from typing import Optional, List
+import pandas as pd
 
+
+DEFAULT_EVENTS_FOLDER = os.path.join(os.environ['HOME'], '.tinyws', 'events')
 
 class DataWharehouse:
     def __init__(self, events_folder: Optional[str] = None, supported_events: Optional[List[str]]=None) -> None:
+        if not events_folder:
+            events_folder = DEFAULT_EVENTS_FOLDER
+            print("Given that no events_folder was given, the default folder will be used: {}".format(events_folder))
+
+        if not os.path.exists(events_folder):
+            os.makedirs(events_folder)
+            
         self.events_folder = events_folder
         self.supported_events = supported_events
 
+    def list_stored_events(self) -> List[str]:
+        os.system("ls -l {}".format(self.events_folder))
 
-    def write_event(self, event, event_name: Optional[str]) -> None:
-        pass
+    def write_event(self, event_name, event: Optional[str]) -> None:
+        """
+        if event is a dict every key will be a column in the parquet file
+        """
+        if type(event) != dict:
+            raise ValueError('event must be a dictionary got {}'.format(type(event)))
+
+        df = pd.DataFrame([event])
+        if os.path.exists(self._parquet_file(event_name)):
+            df = pd.concat([pd.read_parquet(self._parquet_file(event_name)), df])
+        df.to_parquet(self._parquet_file(event_name))
+
+
+    def event(self, event_name: Optional[str]) -> pd.DataFrame:
+        df = pd.read_parquet(self._parquet_file(event_name))
+
+        return df
+
+    def print_event(self, event_name: Optional[str]) -> None:
+        print(self.event(event_name))
+    
+    def _parquet_file(self, event_name):
+        return os.path.join(self.events_folder, event_name + '.parquet')
+        
+
+def main():
+    import fire
+    fire.Fire(DataWharehouse)
+
+if __name__ == "__main__":
+    main()
