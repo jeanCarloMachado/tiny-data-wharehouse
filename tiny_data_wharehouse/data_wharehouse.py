78df8829712085db8511c39b81e96222382a24ee
Jean Carlo Machado <jean.machado@getyourguide.com>
2024-03-02 16:32:54 +0100

    improvemets adding removal command and base folder

--- tiny_data_wharehouse/data_wharehouse.py
+++ tiny_data_wharehouse/data_wharehouse.py
@@ -4,7 +4,8 @@ from typing import Optional, List
 import pandas as pd
 
 
-DEFAULT_EVENTS_FOLDER = os.path.join(os.environ['HOME'], '.tinyws', 'events')
+DEFAULT_FOLDER = os.path.join(os.environ['HOME'], '.tinyws')
+DEFAULT_EVENTS_FOLDER = os.path.join(DEFAULT_FOLDER, 'events')
 
 class DataWharehouse:
     def __init__(self, events_folder: Optional[str] = None, supported_events: Optional[List[str]]=None) -> None:
@@ -15,11 +16,16 @@ class DataWharehouse:
         if not os.path.exists(events_folder):
             os.makedirs(events_folder)
             
+        self.base_folder = DEFAULT_FOLDER
         self.events_folder = events_folder
         self.supported_events = supported_events
 
+
     def list_stored_events(self) -> List[str]:
         os.system("ls -l {}".format(self.events_folder))
+
+    def ls(self):
+        return self.list_stored_events()
 
     def write_event(self, event_name, event: Optional[str]) -> None:
         """
@@ -28,6 +34,7 @@ class DataWharehouse:
         if type(event) != dict:
             raise ValueError('event must be a dictionary got {}'.format(type(event)))
 
+        event['tdw_timestamp'] = pd.Timestamp.now()
         df = pd.DataFrame([event])
         if os.path.exists(self._parquet_file(event_name)):
             df = pd.concat([pd.read_parquet(self._parquet_file(event_name)), df])
@@ -35,9 +42,22 @@ class DataWharehouse:
 
 
     def event(self, event_name: Optional[str]) -> pd.DataFrame:
+        """
+        Reads the data from the event returned as a pandas DataFrame
+        """
+        if not os.path.exists(self._parquet_file(event_name)):
+            raise ValueError('Event {} does not exist'.format(event_name))
+
         df = pd.read_parquet(self._parquet_file(event_name))
 
         return df
+
+    def remove_event(self, event_name: str, dry_run=True) -> None:
+
+        if dry_run:
+            print('Dry run removal of the the event {}. Disable dry run to execute it'.format(event_name))
+
+        os.remove(self._parquet_file(event_name))
 
     def print_event(self, event_name: Optional[str]) -> None:
         print(self.event(event_name))
